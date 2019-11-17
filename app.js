9ba313501dccf63634891ffc0fea249de9c47326
Sherif Hafiz <sherif.hafiz@gmail.com>
2019-11-17 11:14:49 +0000

    Updates app.js to a basic functional application

--- app.js
+++ app.js
@@ -6,26 +6,55 @@ const { buildSchema } = require('graphql');
 const app = express();
 
 app.use(bodyParser.json());
+
+const events = [];
+
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+
+    type Event {
+        _id: ID!, 
+        title: String!, 
+        description: String!, 
+        price: Float!, 
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
 
     type RootMutation { 
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
 
     schema {
-        query: RootQuery,
+        query: RootQuery
         mutation: RootMutation
     }
   `),
   rootValue: {
-    events: () => ['Coding', 'speaking'],
+    events: () => events,
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      console.log(args);
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date().toISOString(),
+      };
+      console.log(event);
+      events.push(event);
+
+      return event;
     },
   },
   graphiql: true,
